ef835aebf7a097df2647a98ec85cb7936a70c24b
sarthak-flobiz <sarthak@flobiz.in>
2021-12-23 18:54:58 +0530

    Setup GitHub action

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // Get token.
+    const token = core.getInput("repo-access-token", { required: true });
+
+    // Check if PR payload exists.
+    if (context.payload.pull_request === undefined) {
+      throw new Error("Can't get pull_request payload. Check you trigger pull_request event");
+    }
+
+    // Extract PR payload.
+    const { assignees, number, user: { login: author, type } } = context.payload.pull_request;
+
+    // Skip assignment if PR is already assigned to someone.
+    if (assignees.length > 0) {
+      core.info(`Assigning author has been skipped since the pull request is already assigned to someone`);
+      return;
+    }
+
+    // Set author=assignee.
+    const octokit = github.getOctokit(token);
+    await octokit.rest.issues.addAssignees({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: number,
+      assignees: [author]
+    });
+    
+    core.info(`@${author} has been assigned to the pull request: #${number}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
